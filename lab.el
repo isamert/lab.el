162568ac545bf705b88c1d57b59a0db14bddbc08
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2024-03-01 17:10:14 +0100

    remove empty line

--- lab.el
+++ lab.el
@@ -123,7 +123,7 @@ Can't be higher than `lab--max-per-page-result-count'."
   'ssh
   "Prefered for cloning repositories into your local."
   :type '(choice (const :tag "SSH" ssh)
-                 (const :tag "HTTPS" https))
+          (const :tag "HTTPS" https))
   :group 'lab)
 
 (defcustom lab-action-handler
@@ -132,7 +132,7 @@ Can't be higher than `lab--max-per-page-result-count'."
 `lab' uses the given function for listing possible actions on a
 selection."
   :type '(choice (const :tag "completing-read" completing-read)
-                 (const :tag "read-multiple-choice" read-multiple-choice))
+          (const :tag "read-multiple-choice" read-multiple-choice))
   :group 'lab)
 
 (defcustom lab-main-branch-name
@@ -455,12 +455,12 @@ function is called if given and the buffer is simply killed."
 (defmacro lab--within-current-project (&rest forms)
   "Let you run FORMS in current projects directory."
   `(let ((default-directory
-           (or
-            (when (boundp 'project-current-inhibit-prompt)
-              project-current-inhibit-prompt)
-            (when (boundp 'project-current-directory-override)
-              project-current-directory-override)
-            default-directory)))
+          (or
+           (when (boundp 'project-current-inhibit-prompt)
+             project-current-inhibit-prompt)
+           (when (boundp 'project-current-directory-override)
+             project-current-directory-override)
+           default-directory)))
      ,@forms))
 
 (defun lab--find-all-repositories-under (path)
@@ -1273,7 +1273,6 @@ Main branch is one the branch names listed in `lab-main-branch-name'."
   (lab--request
    "todos/mark_as_done"
    :%type "POST"))
-
 
 ;;; Formatters & other helpers:
 
